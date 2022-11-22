d1d589d2c2a3e14d00f259963c733a32b45e2639
patelmayank7 <118831883+patelmayank7@users.noreply.github.com>
2022-11-22 21:32:35 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+   $('.food-slider').slick({
+      autoplay:true,
+     slidesToShow:3,
+     slidesToScroll:1,
+     prevArrow:".prev-btn",
+     nextArrow:".next-btn",
+     responsive:[
+        {
+           breakpoint:992,
+           settings:{
+            slidesToShow:2,
+           }
+        },
+        {
+         breakpoint:768,
+         settings:{
+          slidesToShow:1,
+         }
+      }
+     ]
+
+   });
+
+   $('.nav-trigger').click(function(){
+      $('.site-content-wrapper').toggleClass('scaled');
+   })
+   function myFunction() {
+      var dots = document.getElementById("dots");
+      var moreText = document.getElementById("more");
+      var btnText = document.getElementById("myBtn");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+    
+});
